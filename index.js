0af5244f01a13204a318fddf5d0a122467a43764
Marcio Godoi <souzagodoi@gmail.com>
2017-08-16 16:32:57 -0300

    Adding parameters to the lambda

--- index.js
+++ index.js
@@ -3,5 +3,8 @@ console.log('Loading event');
 
 exports.handler = function(event, context, callback) {
   console.log('My first lambda published by command line');
+  console.log('Nome: ' + event.nome);
+  console.log('Sobrenome: ' + event.sobrenome);
+  console.log('Empresa: ' + event.empresa);
   callback(null, {"Message":"My first lambda published by command line"});  // SUCCESS with message
 };
